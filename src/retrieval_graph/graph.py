d4e2221c22aff48d0b5e5d9ff79e125beb7b310d
sasinomalabs <sasi@nomasec-labs.com>
2025-02-04 12:03:33 +0200

    Update graph.py

--- src/retrieval_graph/graph.py
+++ src/retrieval_graph/graph.py
@@ -24,6 +24,7 @@ from retrieval_graph.utils import format_docs, get_message_text, load_chat_model
 import subprocess
 import socket
 import requests
+import re
 
 # Define the function that calls the model
 
